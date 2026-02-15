ece2859429d18f0e1bfff2419ebe35bf1257ed59
Dr. J. K. Verma <jk-verma@ieee.org>
2026-02-15 23:00:38 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -86,7 +86,7 @@ const REASON = {
     LATE_COMP_TYPE1: "Late Compensation—Type I",
     LATE_COMP_TYPE2: "Late Compensation—Type II",
     REJECT: "REJECT",
-    PENDING: "Pending Punch-Out",
+    PENDING: "Pending Puch-Out",
     CLOSED: "Closed Holiday",
     SPECIAL: "Special Leave"
 };
