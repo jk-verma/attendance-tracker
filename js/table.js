e2ee2e69e412bf3ff81df0e203187de8b6cdc9a9
Dr. J. K. Verma <jk-verma@ieee.org>
2026-02-16 00:46:02 +0530

    Update table.js

--- js/table.js
+++ js/table.js
@@ -21,8 +21,8 @@ function renderTable() {
 
         const tr = document.createElement("tr");
 
-        if (isPendingOutReason(record.reason)) {
-            tr.className = "status-pending";
+        if (isMissingOutReason(record.reason)) {
+            tr.className = "status-missing";
         } else if (record.reason === REASON.CLOSED || record.reason === REASON.SPECIAL) {
             tr.className = "status-neutral";
         } else if (record.status === STATUS.COMPLIANT) {
